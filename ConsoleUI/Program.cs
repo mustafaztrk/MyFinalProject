123ddd2d9e8b7cc859d5ea1d6a4ad3559f1cd276
Mustafa <mustafaozturk0671@gmail.com>
2021-02-10 03:11:06 +0300

    10 Şubat version 3

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -10,7 +10,12 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
-            //PoductTest();
+           // ProductTest();
+           //CategoryTest();
+        }
+
+        private static void CategoryTest()
+        {
             CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
             foreach (var category in categoryManager.GetAll())
             {
@@ -22,9 +27,9 @@ namespace ConsoleUI
         {
             ProductManager productManager = new ProductManager(new EfProductDal());
 
-            foreach (var product in productManager.GetAllByCategoryId(2))
+            foreach (var product in productManager.GetProductDetails())
             {
-                Console.WriteLine(product.ProductName);
+                Console.WriteLine(product.ProductName+"/"+product.CategoryName);
             }
         }
     }
